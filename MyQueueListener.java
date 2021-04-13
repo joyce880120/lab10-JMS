a82cdf804b44e5caa3f728455436f1c789b3d7fc
Marty Barrett <47185849+martyBarr@users.noreply.github.com>
2021-04-13 10:32:14 -0400

    Add files via upload

---
+++ MyQueueListener.java
@@ -0,0 +1,38 @@
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+// This creates the mapping of this MessageListener to the apropriate Queue
+@MessageDriven(mappedName = "jms/myQueue", activationConfig = {
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class MyQueueListener implements MessageListener {
+
+    public MyQueueListener() {
+    }
+
+    /*
+     * When a message is available in jms/myQueue, then onMessage is called.
+     */
+    public void onMessage(Message message) {
+        try {
+            String tmt = "";
+            // Since there can be different types of Messages, make sure this is the right type.
+            if (message instanceof TextMessage) {
+                TextMessage tm = (TextMessage) message;
+                tmt = tm.getText();
+                System.out.println("MyQueueListener received: " + tmt);
+            } else {
+                System.out.println("I don't handle messages of this type");
+            }
+        } catch (JMSException e) {
+            System.out.println("JMS Exception thrown" + e);
+        } catch (Throwable e) {
+            System.out.println("Throwable thrown" + e);
+        }
+    }
+}
